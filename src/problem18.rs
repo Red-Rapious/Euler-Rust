47fa36c9494c88b93548e25ea75f03c473a7f8e5
Red Rapious <red.rapious@gmail.com>
2023-07-05 15:40:20 +0200

    Moved more inputs to separate files

--- src/problem18.rs
+++ src/problem18.rs
@@ -11,6 +11,7 @@ Find the maximum total from top to bottom of the triangle below:
 NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route. However, Problem 67, is the same challenge with a triangle containing one-hundred rows; it cannot be solved by brute force, and requires a clever method! */
 
 use std::cmp::max;
+use std::fs;
 
 fn shortest_path_length(triangle: &Vec<Vec<usize>>, x: usize, y: usize) -> usize {
     if y == triangle.len() - 1 {
@@ -29,27 +30,14 @@ pub fn problem18() -> usize {
 }
 
 fn get_triangle() -> Vec<Vec<usize>> {
-    let input =
-"75
-95 64
-17 47 82
-18 35 87 10
-20 04 82 47 65
-19 01 23 75 03 34
-88 02 77 73 07 63 67
-99 65 04 28 06 16 70 92
-41 41 26 56 83 40 80 70 33
-41 48 72 33 47 32 37 16 94 29
-53 71 44 65 25 43 91 52 97 51 14
-70 11 33 28 77 73 17 78 39 68 17 57
-91 71 52 38 17 14 91 43 58 50 27 29 48
-63 66 04 68 89 53 67 30 73 16 69 87 40 31
-04 62 98 27 23 09 70 98 73 93 38 53 60 04 23";
+    let file_path = "problem18.txt";
+    let contents = fs::read_to_string(file_path)
+        .expect("Unable to read file.");
 
     let mut triangle = vec![];
 
     let mut i = 0;
-    for line in input.split("\n") {
+    for line in contents.split("\n") {
         triangle.push(vec![]);
         for number in line.split(" ") {
             triangle[i].push(number.parse().unwrap());
